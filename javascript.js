1bbfbc2a8c4cba17c80a1be24ceb367d315cea08
KMIHALY <kaloczy.mihaly@gmail.com>
2018-08-15 11:50:46 +0200

    coaches ready!

--- javascript.js
+++ javascript.js
@@ -1,11 +1,23 @@
 'use strict'
 
-// $(document).ready(function () {
-//     $('[data-toggle="tooltip"]').tooltip();
-// });
+$(document).ready(function () {
+     $('[data-toggle="tooltip"]').tooltip();
+ });
 
 function openImg(imgs) {
     $("#imgtext").text(imgs.alt);
     $("#expandedImg").attr("src", imgs.src);
-    $(".containerImg").css("display","block");
+    $(".containerImg").css("display", "block");
 }
+
+$(document).ready(toggleBrand);
+
+$(window).resize(toggleBrand);
+
+function toggleBrand() {
+    if ($(window).width() < 400) {
+        $("#brand").hide();
+    } else {
+        $("#brand").show();
+    }
+};
