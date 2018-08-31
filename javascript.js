a37b3dcf1a8bbbce2f269a59682aff1a099a0e5f
KMIHALY <kaloczy.mihaly@gmail.com>
2018-08-31 10:10:47 +0200

    waypoints added, home-> about still error

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 'use strict'
 
 $(document).ready(function () {
-     $('[data-toggle="tooltip"]').tooltip();
- });
+    $('[data-toggle="tooltip"]').tooltip();
+});
 
 function openImg(imgs) {
     $("#imgtext").text(imgs.alt);
@@ -14,9 +14,68 @@ $(document).ready(toggleBrand);
 $(window).resize(toggleBrand);
 
 function toggleBrand() {
-    if ($(window).width() < 420) {
+    if ($(window).width() < 460) {
         $("#brand").hide();
     } else {
         $("#brand").show();
     }
 };
+
+// creating waypoints
+
+
+
+const waypointIds = ['#home', '#about', '#coaches', '#services', '#survivalKits', '#events', '#contact'];
+
+const listItemFinder = (waypointId) => {
+    let waypointIdArray = waypointId.split("");
+    waypointIdArray.splice(1, 0, "li-");
+    let listItemIdString = waypointIdArray.join("");
+    return listItemIdString;
+};
+
+let makeListItemHighlighted = (currentValue) => {
+    waypointIds.map(
+        (currentValue) => {
+            // $(listItemFinder(currentValue)).css('color', '#f7e4bc');
+            $(listItemFinder(currentValue)).css('color', '#d7c8a8');
+        });
+    $(listItemFinder(currentValue)).css('color', 'white');
+}
+
+$(String(waypointIds[0])).waypoint(
+    () => { 
+        makeListItemHighlighted(listItemFinder(waypointIds[0])); 
+        document.getElementById("li-home").style.color = "white";
+    },
+{ offset: -1 }  
+);
+
+waypointIds.map(
+    (currentValue) => {
+        $(currentValue).waypoint(
+            () => { makeListItemHighlighted(currentValue); },
+            { offset: -5 }
+        )
+    }
+);
+
+waypointIds.map(
+    (currentValue) => {
+        $(currentValue).waypoint(
+            () => { makeListItemHighlighted(currentValue); },
+            { offset: 60 }
+        )
+    }
+);
+
+waypointIds.map(
+    (currentValue) => {
+        $(currentValue).waypoint(
+            () => { makeListItemHighlighted(currentValue); },
+            { offset: 'bottom-in-view' }
+        )
+    }
+);
+
+
